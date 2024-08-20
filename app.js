6a60309f04d0158c43f975682fe28d507fae800c
Bruno Penner <brunopenner01@gmail.com>
2024-08-20 08:54:39 +0800

    #addEventListener

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked the h1');
 }
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function() {
+    alert('CLICKED');
+});
+
+function twist() {
+    console.log("TWIST!");
+}
+function shout() {
+    console.log("SHOUT!");
+}
+
+const tasBtn = document.querySelector('#tas');
+
+// The following codes don't work, only the last function will work, since click is a property and only accepts one value
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// AddEventListener is better since you can add as many callback functions as you like
+//  and you can also pass more parameters, such as for the function to run only once
+tasBtn.addEventListener('click', twist);
+tasBtn.addEventListener('click', shout);

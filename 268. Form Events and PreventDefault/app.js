f8a14aacb2687b646ce738cf1856647706a365b2
Bruno Penner <brunopenner01@gmail.com>
2024-08-22 13:01:50 +0800

    Practice With Form Events & PreventDefault

--- 268. Form Events and PreventDefault/app.js
+++ 268. Form Events and PreventDefault/app.js
@@ -1,5 +1,17 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats")
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    console.log("SUBMITTED!!!!")
+    console.log("SUBMITTED!!!!");
+
+    if (input.value != '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        list.appendChild(li);
+        input.value = "";
+    } else {
+        alert("Cat name can't be empty!");
+    }
+    
 })

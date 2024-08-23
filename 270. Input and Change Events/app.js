edb83d143d2fbdef64adbdb189aa866407f83893
Bruno Penner <brunopenner01@gmail.com>
2024-08-23 11:49:30 +0800

    #Coding Exercise 62: Input Event Practice

--- 270. Input and Change Events/app.js
+++ 270. Input and Change Events/app.js
@@ -1,6 +1,12 @@
 const input = document.querySelector('input');
 const h1 = document.querySelector("h1");
+const welcome = document.querySelector("h3");
+const username = document.querySelector("#username");
 
 input.addEventListener('input', function(e) {
     h1.innerText = input.value;
 }) 
+
+username.addEventListener('input', function (e) {
+    welcome.textContent = "Welcome, " + this.value;
+})

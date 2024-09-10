97e99eaddb10ace166df39af00fba7fafc304087
Bruno Penner <brunopenner01@gmail.com>
2024-09-10 08:06:55 +0800

    #285. The Async Keyword

--- 284. Creating Promises/app.js
+++ 284. Creating Promises/app.js
@@ -1,23 +1,23 @@
-// const fakeRequest = (url) => {
-//     return new Promise((resolve, reject) => {
-//         const rand = Math.random();
-//         setTimeout(() => {
-//             if (rand < 0.7) {
-//                 resolve('YOUR FAKE DATA HERE');
-//             }
-//             reject('Request Error!');
-//         }, 1000)
-//     })
-// }
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if (rand < 0.7) {
+                resolve('YOUR FAKE DATA HERE');
+            }
+            reject('Request Error!');
+        }, 1000)
+    })
+}
 
-// fakeRequest('dogs/1')
-//     .then((data) => {
-//         console.log("DONE WITH REQUEST!");
-//         console.log('data is:', data);
-//     })
-//     .catch((err) => {
-//         console.log('OH NO!', err);
-//     })
+fakeRequest('dogs/1')
+    .then((data) => {
+        console.log("DONE WITH REQUEST!");
+        console.log('data is:', data);
+    })
+    .catch((err) => {
+        console.log('OH NO!', err);
+    })
 
 
 

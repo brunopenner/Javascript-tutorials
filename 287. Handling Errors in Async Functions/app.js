a0f8911b16ccd2773290230489d83e63d119ab38
Bruno Penner <brunopenner01@gmail.com>
2024-09-16 09:18:05 +0800

    #287. Handling Errors in Async Functions

---
+++ 287. Handling Errors in Async Functions/app.js
@@ -0,0 +1,18 @@
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 2000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+
+async function makeTwoRequests() {
+    let data1 = await fakeRequest('/page1');
+    console.log(data1);
+}

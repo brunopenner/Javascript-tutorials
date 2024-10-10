5fe288683908b9e0fd916414d2fbc64aab4b160e
Bruno Penner <brunopenner01@gmail.com>
2024-10-10 09:36:59 +0800

    #609. The Rules of JSX

--- Section 61. JSX in Detail/608. Importing and Exporting Components/src/App.jsx
+++ Section 61. JSX in Detail/608. Importing and Exporting Components/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Greeter from './Greeter.js';
 import Dog from './Dog.js';
+import LoginForm from './LoginForm.js';
 
 export function App(props) {
   return (
     <div className='App'>
       <Greeter/>
       <Dog/>
-      <Dog/>
-      <Dog/>
-      <Greeter/>
+      <LoginForm/>
     </div>
   );
 }

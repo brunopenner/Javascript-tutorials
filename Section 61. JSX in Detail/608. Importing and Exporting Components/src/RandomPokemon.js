0b2d3383ac025ca43608e3dcfc9b05061ef4e035
Bruno Penner <brunopenner01@gmail.com>
2024-10-21 09:20:27 +0800

    #615. Random PokeCard Exercise - adding styles

--- Section 61. JSX in Detail/608. Importing and Exporting Components/src/RandomPokemon.js
+++ Section 61. JSX in Detail/608. Importing and Exporting Components/src/RandomPokemon.js
@@ -1,5 +1,10 @@
+import "./RandomPokemon.css";
+
 export default function RandomPokemon() {
     const RandomNumber = (Math.floor(Math.random() * 150)) + 1;
     const pokemonImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${RandomNumber}.png`
-    return <img src={pokemonImgUrl} alt="Random Pokemon" />;
+    return <div className="RandomPokemon">
+      <h1>Pokemon #{RandomNumber}</h1>
+      <img src={pokemonImgUrl} alt="Random Pokemon" />
+    </div>
   }
